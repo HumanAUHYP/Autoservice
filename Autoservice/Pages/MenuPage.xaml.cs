d085a8812617ae38f5b59074d94aedcecb829435
Human <ainur.hak2004@gmail.com>
2023-02-16 17:13:09 +0300

    Добавьте файлы проекта.

---
+++ Autoservice/Pages/MenuPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autoservice.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        MainWindow globalMainWindow;
+        public MenuPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+        }
+
+        private void btnClients_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnServices_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPerformServices_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Autoservice/Pages/PerformServicesTablePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autoservice.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PerformServicesTablePage.xaml
+    /// </summary>
+    public partial class PerformServicesTablePage : Page
+    {
+        public PerformServicesTablePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPerformService_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddPerformServicePage());
+        }
+    }
+}
